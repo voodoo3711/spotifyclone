77f548b5fef888cef5fe76a281b311ac2d2d89d7
voodoo3711 <42246848+voodoo3711@users.noreply.github.com>
2020-08-11 23:05:12 +0530

    appjs

--- spotifyclone/src/App.js
+++ spotifyclone/src/App.js
@@ -5,7 +5,7 @@ import Login from "./Login";
 function App() {
   return ( 
     
-    <div className="app">
+    <div className="login">
       
       <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
       alt=""
--- spotifyclone/src/Login.js
+++ spotifyclone/src/Login.js
@@ -13,3 +13,4 @@ function Login() {
 
   );
 }
+export default Login;
