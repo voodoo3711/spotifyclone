1e279438fe747eda8ba37f278797c015b879e157
voodoo3711 <42246848+voodoo3711@users.noreply.github.com>
2020-08-11 22:50:36 +0530

    BUTTON CREATED

---
+++ spotifyclone/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Login from "./Login";
+
+function App() {
+  return ( 
+    
+    <div className="app">
+      
+      <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+      alt=""
+      />
+      <button>LOGIN WITH SPOTIFY</button>
+    </div>
+);
+}
+
+export default App;
